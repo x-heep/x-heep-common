fcba91e0a95a76e3cc850191d11a454f0f357718
Michele Caon <michele.caon@epfl.ch>
2026-06-02 18:33:09 +0200

    Revert last commit

--- hw/mem/xheep_mem_demux.sv
+++ hw/mem/xheep_mem_demux.sv
@@ -65,8 +65,8 @@ module xheep_mem_demux #(
   // INTERNAL SIGNALS
   // ----------------
   logic [VrfLineWordAddrW-1:0]           vrf_word_addr[DELAY+1];
-  logic [  VrfLineWordNum-1:0][S_DW-1:0] vrf_rdata;
-  logic [  VrfLineWordNum-1:0][S_DW-1:0] vrf_wdata;
+  logic [  VrfLineWordNum-1:0][M_DW-1:0] vrf_rdata;
+  logic [  VrfLineWordNum-1:0][M_DW-1:0] vrf_wdata;
 
   // In case the VRF data width is larger (i.e., a multiple) than the OBI data
   // width, the OBI word offset within the VRF word must be delayed as well.
